e9edbc0abcac864db33a1bed1924e14f8f404677
Val Cohen <val@valcohen.com>
2019-03-09 15:16:21 -0800

    Generate & display Perlin noise

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate")) {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;    // TODO: "textureScale" for non-noise sources?
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
