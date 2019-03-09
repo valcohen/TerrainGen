ecb2f962003fd650b33e3fdef76e5757a95d8629
Val Cohen <val@valcohen.com>
2019-03-09 15:45:22 -0800

    Add octaves, persistence, lacunarity

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,10 +8,17 @@ public class MapGenerator : MonoBehaviour {
     public int mapHeight;
     public float noiseScale;    // TODO: "textureScale" for non-noise sources?
 
+    public int octaves;
+    public float persistence;
+    public float lacunarity;
+
     public bool autoUpdate;
 
     public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(
+            mapWidth, mapHeight, noiseScale,
+            octaves, persistence, lacunarity
+        );
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
