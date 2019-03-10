6d515adcf7a8cefd454ba706994a335b1c387f05
Val Cohen <val@valcohen.com>
2019-03-09 18:17:30 -0800

    Add UVs

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -26,7 +26,7 @@ using UnityEngine;
 
 public static class MeshGenerator {
 
-    public static void GenerateTerrainMesh(float[,] heightMap) {
+    public static MeshData GenerateTerrainMesh(float[,] heightMap) {
         int width  = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
 
@@ -52,6 +52,10 @@ public static class MeshGenerator {
                 meshData.vertices[vertexIndex] = new Vector3(
                     topLeftX + x, heightMap[x,y], topLeftZ - y
                 );
+                meshData.uvs[vertexIndex] = new Vector2(
+                    x / (float) width,
+                    y / (float) height
+                );  // normalize to 0..1
 
                 // ignore right & bottom edges
                 if (x < (width -1) && y < (height - 1) ) {
@@ -71,17 +75,21 @@ public static class MeshGenerator {
                 vertexIndex++;
             }
         }
+
+        return meshData;
     }
 }
 
 public class MeshData {
     public Vector3[] vertices;
     public int[] triangles;
+    public Vector2[] uvs;
 
     int triangleIndex;
 
     public MeshData(int meshWidth, int meshHeight) {
         vertices  = new Vector3[meshWidth * meshHeight];
+        uvs = new Vector2[meshWidth * meshHeight];
         triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
     }
 
@@ -90,5 +98,15 @@ public class MeshData {
         triangles[triangleIndex + 1] = b;
         triangles[triangleIndex + 2] = c;
         triangleIndex += 3;
+    }
+
+    public Mesh CreateMesh () {
+        Mesh mesh = new Mesh();
+        mesh.vertices   = vertices;
+        mesh.triangles  = triangles;
+        mesh.uv         = uvs;
+        mesh.RecalculateNormals();
+
+        return mesh;
     }
 }
